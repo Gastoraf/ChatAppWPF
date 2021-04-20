d1b745db8d75633a9d7d736bfc610d646b07b996
Алексей Рудианов <55290713+Gastoraf@users.noreply.github.com>
2021-04-20 18:25:42 +0300

    ws

---
+++ UsersApp/ViewModel/WindowsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UsersApp
+{
+    
+    /// <summary>
+    /// The view model for the custom flat window
+    /// </summary>
+    
+    public class WindowsViewModel : BaseViewModel
+    {
+        #region Private Member
+        /// <summary>
+        /// The window this view model controls
+        /// </summary>
+        private Window mWindow;
+        #endregion
+
+        #region Public Properties
+
+        public string Test { get; set; } = "Hello! It's chat.";
+
+        //public ApplicationPage CurrentPage { get; set; } = ApplicationPage.login.Login;
+        #endregion
+
+        #region Constuctor
+        /// <summary>
+        /// Defoult constructor
+        /// </summary>
+        public WindowsViewModel(Window window)
+        {
+            mWindow = window;
+
+            
+        }
+        #endregion
+
+    }
+}

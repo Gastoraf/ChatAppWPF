08ac4526a40a6265718c2f495ba0caa09d1261e4
Алексей Рудианов <55290713+Gastoraf@users.noreply.github.com>
2021-04-20 18:40:59 +0300

    ss

---
+++ UsersApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace UsersApp
+{
+
+    public partial class MainWindow : Window
+    {
+        ///
+        public MainWindow()
+        {
+            //this.DataContext = new WindowsViewModel(this);
+
+            InitializeComponent();
+
+            MainFrame.Navigate(new Pages.ChatPage());
+
+
+        }
+
+        //private void Btn1(object sender, RoutedEventArgs e)
+        //{
+        //    Main.Content = new Pages.LoginPage();
+        //}
+        //private void Btn2(object sender, RoutedEventArgs e)
+        //{
+        //    Main.Content = new Pages.LogupPage();
+        //}
+
+    }
+}
